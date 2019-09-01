a422851af03f7f8fa345e08d4e993ef92e9d324c
lvbin <lb@kuaihuoyun.com>
2019-09-01 17:56:07 +0800

    :hankey: pwa simple todo  fix bug

--- .umirc.ts
+++ .umirc.ts
@@ -15,12 +15,29 @@ const config: IConfig = {
       {
         antd: true,
         dva: true,
-        dynamicImport: { webpackChunkName: true },
+        dynamicImport: {
+          webpackChunkName: true,
+          level: 0,
+        },
         title: '掘金 - juejin.im - 一个帮助开发者成长的社区',
         dll: true,
         locale: {
           enable: true,
           default: 'en-US',
+          // default true, when it is true, will use `navigator.language` overwrite default
+          baseNavigator: true,
+        },
+        // pwa: {
+        //   manifestOptions: {
+        //     srcPath: 'src/manifest.json',
+        //   },
+        //   workboxPluginMode: 'InjectManifest',
+        //   workboxOptions: {
+        //     importWorkboxFrom: 'local',
+        //   },
+        // },
+        manifest: {
+          basePath: '/',
         },
         routes: {
           exclude: [
